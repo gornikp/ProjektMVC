a859b3754a749242b959d3c53d1423a1f1b1beb5
Potrg <gornikp94@gmail.com>
2015-11-16 01:15:50 +0100

    Utworzyłem bazę danych online (więc nie będzie problemu że coś się nie skopiowało i że ktoś nie ma jakiś danych z bazy albo że sciezka do bazy danych jest nieprawidłowa)

---
+++ WypasionaKsiegarniaMVC/Models/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypasionaKsiegarniaMVC.Models
+{
+                         /*
+                         * Mechanizm Migrations
+                         * 
+                         * 1. W Package Manager Console wlaczyc migracje: enable-migrations
+                         * 2. Stworzenie wersji bazy add-migration nazwa_migracji
+                         * 3. Naniesie zmian na baze: update-database
+                         */
+    class ProductContext : DbContext
+    {
+        public ProductContext() : base("MyCS") { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}
