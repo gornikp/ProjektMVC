5dc300a3dc0349c570df0de3640b992d13ea80ae
ptaszysko <weronika.sawicka.9@gmail.com>
2016-01-24 15:12:17 +0100

    Dodanie widoku listy autorów  w widoku i edycji produktu

---
+++ WypasionaKsiegarniaMVC/Models/ProductAuthorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WypasionaKsiegarniaMVC.Models
+{
+    public class ProductAuthorViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> AllAuthorsList { get; set; }
+
+        private List<int> _selectedAuthors;
+        public List<int> SelectedAuthors
+        {
+            get
+            {
+                if (_selectedAuthors == null)
+                {
+                    _selectedAuthors = Product.Authors.Select(i => i.AuthorsID).ToList();
+                }
+                return _selectedAuthors;
+            }
+            set { _selectedAuthors = value; }
+        }
+    }
+}
